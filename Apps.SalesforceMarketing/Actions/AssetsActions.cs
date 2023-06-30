2080d22fa4f730e59fa1d13a36c918e75fba1e28
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-06-30 17:14:20 +0300

    Add project files.

---
+++ Apps.SalesforceMarketing/Actions/AssetsActions.cs
@@ -0,0 +1,26 @@
+﻿using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.SalesforceMarketing.Actions
+{
+    public class AssetsActions
+    {
+        //[Action("List all assets", Description = "List all assets")]
+        //public async Task<GetChatUsersResponse> ListAssets(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders,
+        //    [ActionParameter] GetChatUsersRequest input)
+        //{
+        //    var client = new MSTeamsClient(authenticationCredentialsProviders);
+        //    var members = await client.Me.Chats[input.ChatId].Members.GetAsync();
+        //    return new GetChatUsersResponse()
+        //    {
+        //        Members = members.Value.Select(m => new ChatMemberDto(m))
+        //    };
+        //}
+    }
+}
---
+++ Apps.SalesforceMarketing/SalesforceRequest.cs
@@ -0,0 +1,20 @@
+﻿using Apps.SalesforceMarketing;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.SalesforceMarketing
+{
+    public class SalesforceRequest : RestRequest
+    {
+        public SalesforceRequest(string endpoint, Method method, IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders) : base(endpoint, method)
+        {
+            var token = authenticationCredentialsProviders.First(p => p.KeyName == "Authorization").Value;
+            this.AddHeader("Authorization", $"{token}");
+        }
+    }
+}
